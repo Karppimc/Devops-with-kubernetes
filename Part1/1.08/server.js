c68a4c7747cb9e41a03c5daf39038e302ae9c68f
Santeri <santtukarppi@gmail.com>
2024-12-23 17:41:17 +0200

    1.08 done

---
+++ Part1/1.08/server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const crypto = require('crypto');
+
+const PORT = process.env.PORT || 3001;
+
+// Generate a random hash for the application (stored for the process lifetime)
+const appHash = crypto.randomUUID();
+
+const server = http.createServer((req, res) => {
+    const requestHash = crypto.randomUUID();
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(`
+        <h1>Log Output Application</h1>
+        <p>Application Hash: ${appHash}</p>
+        <p>Request Hash: ${requestHash}</p>
+    `);
+});
+
+server.listen(PORT, () => {
+    console.log(`Server started in port ${PORT}`);
+});
