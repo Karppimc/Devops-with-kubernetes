05c27c9298c40c7cfeac505159af063634403f38
Santeri <santtukarppi@gmail.com>
2025-01-24 17:31:12 +0200

    Test for automation

--- Part4/4.07/log-output/server.js
+++ Part4/4.07/log-output/server.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const express = require('express');
-const axios = require('axios'); 
+const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -21,7 +21,7 @@ app.get('/', async (req, res) => {
 
 
     try {
-        const response = await axios.get('http://pingpong-svc:80/pingpong');
+        const response = await axios.get(`http://${process.env.PINGPONG_HOST}:${process.env.PINGPONG_PORT}/pingpong`);
         pongCount = response.data.pongCount || "N/A";
     } catch (err) {
         console.error('Failed to fetch pong count:', err.message);
